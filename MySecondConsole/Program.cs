9614b356e61c2033346d9d04b11bf0a18f21d82f
Ivan K <49645913+justkiddo@users.noreply.github.com>
2022-11-28 17:37:02 +0300

    Add files via upload

---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+double Pi = 3.141592653;
+const double e = 1.414213562373095048;
+
+Console.WriteLine(Pi + "\n");
+Console.WriteLine(e + "\n");
+
+int a = 3;
+byte b = (byte)a;
+
+int[] array = new int[3];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("\nEnter " + (i+1) + " number \n");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+double midA = ((double)array[0] + (double)array[1] + (double)array[2]) / array.Length ;
+
+Console.WriteLine("\n" + midA);
+
+Console.WriteLine("\nEnter your name\n");
+String yName = "\nHi, " + Console.ReadLine();
+
+Console.WriteLine(yName);
---
+++ Test/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+int[] source = { 5, 4, 7, 2, 9 };
+int index = 0;
+
+for (int i = 0; i< source.Length; i++)
+{
+    Console.WriteLine(source[i]);
+}
+
+for (int i = index; i < source.Length - 1; i++)
+{
+    source[i] = source[i + 1];
+}
+Array.Resize(ref source, source.Length - 1);
+
+
+Console.WriteLine(String.Join(", ", source));
+
+
+
+//int[] array = new int[3];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    Console.WriteLine("Введите " + i + " число");
+//    array[i] = Convert.ToInt32(Console.ReadLine());
+//}
