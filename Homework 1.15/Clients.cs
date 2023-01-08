388a5d125a1f7f13de6c0286595fc5db7525c50a
Ivan K <ki93@yandex.ru>
2023-01-08 16:23:40 +0300

    1.15

---
+++ Homework 1.15/Clients.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1._15
+{
+    internal class Clients
+    {
+        public int id;
+        public int year;
+        public int month;
+        public int duration;
+
+
+        public Clients(int id, int year, int month, int duration)
+        {
+            this.id = id;
+            this.year = year;
+            this.month = month;
+            this.duration = duration;
+        }
+        public Clients()
+        {
+        }
+
+        public override string ToString() => $"id - {id} year - {year} month - {month} duration - {duration}";
+
+
+    }
+}
---
+++ Homework 1.15/Program.cs
@@ -0,0 +1,69 @@
+﻿
+
+//1.Дана целочисленная последовательность.Извлечь из нее все четные 
+//    отрицательные числа, поменяв порядок извлеченных чисел на обратный.
+
+
+using Homework_1._15;
+using System;
+
+var intArr = new int[] {-5,-6,-8,-4,4,6,8,9,5,3,1,7,11,-29,-34};
+
+
+IEnumerable<int> intFilter = intArr.Where(w => w < 0 && w % 2 == 0).Reverse();
+
+foreach (int num in intFilter)
+{
+    Console.WriteLine(num);
+}
+
+//2.Дана последовательность непустых строк A. Получить последовательность символов, каждый элемент 
+//    которой является начальным символом соответствующей строки
+//из A. Порядок символов должен быть обратным по отношению к порядку элементов исходной последовательности.
+
+
+
+
+var stringArr = new string[] {"first", "second", "third", "forth", "fifth", "etc" };
+
+IEnumerable<string> sFilter = stringArr.Select(w => w.First().ToString()).Reverse();
+
+
+foreach (string s in sFilter)
+{
+    Console.WriteLine(s);
+}
+
+//3.Исходная последовательность содержит сведения о
+//клиентах фитнес-центра. Каждый элемент последовательности включает следующие целочисленные поля:
+//< Год > < Номер месяца > < Продолжительность
+//занятий(в часах) > < Код клиента >
+//Определить год, в котором суммарная продолжительность
+//занятий всех клиентов была наибольшей, и вывести этот год
+//и наибольшую суммарную продолжительность. Если таких
+//годов было несколько, то вывести наименьший из них. 
+
+
+
+List<Clients> clients = new List<Clients>() { new (11, 2015, 12, 45),
+                                              new (13, 2015, 5, 45),
+                                              new (51, 2016, 6, 46),
+                                              new (61, 2017, 7, 47),
+                                              new (11, 2016, 3, 48),
+                                              new (15, 2016, 5, 55)
+};
+
+
+
+var filteredCl = clients.GroupBy(y => y.year).Select(group => new Clients
+{
+    year = group.Key,
+    duration = group.Sum(d => d.duration)
+});
+
+var filteredFcl = filteredCl.Where(d => d.duration == filteredCl.Max(d => d.duration));
+
+foreach (Clients c in filteredFcl)
+{
+    Console.WriteLine(c.ToString());
+}
