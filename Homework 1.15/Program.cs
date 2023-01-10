69a531cb74021bdaee051d38ddccdd6a812d51e6
Ivan K <ki93@yandex.ru>
2023-01-10 18:54:29 +0300

    1.15

--- Homework 1.15/Program.cs
+++ Homework 1.15/Program.cs
@@ -1,14 +1,10 @@
-﻿
+﻿using Homework_1._15;
+using System;
 
 //1.Дана целочисленная последовательность.Извлечь из нее все четные 
 //    отрицательные числа, поменяв порядок извлеченных чисел на обратный.
 
-
-using Homework_1._15;
-using System;
-
 var intArr = new int[] {-5,-6,-8,-4,4,6,8,9,5,3,1,7,11,-29,-34};
-
 
 IEnumerable<int> intFilter = intArr.Where(w => w < 0 && w % 2 == 0).Reverse();
 
@@ -21,13 +17,9 @@ foreach (int num in intFilter)
 //    которой является начальным символом соответствующей строки
 //из A. Порядок символов должен быть обратным по отношению к порядку элементов исходной последовательности.
 
-
-
-
 var stringArr = new string[] {"first", "second", "third", "forth", "fifth", "etc" };
 
 IEnumerable<string> sFilter = stringArr.Select(w => w.First().ToString()).Reverse();
-
 
 foreach (string s in sFilter)
 {
@@ -43,8 +35,6 @@ foreach (string s in sFilter)
 //и наибольшую суммарную продолжительность. Если таких
 //годов было несколько, то вывести наименьший из них. 
 
-
-
 List<Clients> clients = new List<Clients>() { new (11, 2015, 12, 45),
                                               new (13, 2015, 5, 45),
                                               new (51, 2016, 6, 46),
@@ -53,17 +43,15 @@ List<Clients> clients = new List<Clients>() { new (11, 2015, 12, 45),
                                               new (15, 2016, 5, 55)
 };
 
-
-
 var groupedCl = clients.GroupBy(y => y.year).Select(group => new Clients
 {
     year = group.Key,
     duration = group.Sum(d => d.duration)
 });
 
-var folteredCl = groupedCl.Where(d => d.duration == groupedCl.Max(d => d.duration));
+var filteredCl = groupedCl.Where(d => d.duration == groupedCl.Max(d => d.duration));
 
-foreach (Clients c in folteredCl)
+foreach (Clients c in filteredCl)
 {
     Console.WriteLine(c.ToString());
 }
