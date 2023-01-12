e80193f04f91ec755428bf1b4ba274c763396975
Ivan K <ki93@yandex.ru>
2023-01-13 00:41:57 +0300

    1.16

---
+++ Homework 1.16/DirectoryWorker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1._16
+{
+    internal class DirectoryWorker
+    {
+        private string path = string.Empty;
+
+        public void FilesInDirectory()
+        {
+            DirectoryInfo directory= new DirectoryInfo(path);
+            var value = directory.GetFiles();
+            Console.WriteLine(value.Length);
+        }
+
+        public void FilesInCat()
+        {
+            var fileCount = (from file in Directory.EnumerateFiles(path, "" , SearchOption.AllDirectories)
+                             select file).Count();
+            Console.WriteLine($"there is {fileCount} files in {path}");
+            Console.WriteLine();
+        }
+
+        public void DirectoryFileInfo()
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            var value = directory.GetFiles();
+            foreach(var file in value)
+            {
+                Console.WriteLine(file.Name);
+                Console.WriteLine($"{file.Length} bytes");
+                Console.WriteLine();
+            }
+        }
+
+        public void CountCatExtFiles(string fileExtension)
+        {
+                var fileCount = (from file in Directory.EnumerateFiles(path, fileExtension, SearchOption.AllDirectories)
+                                 select file).Count();
+                Console.WriteLine($"there is {fileCount} files in {path} with {fileExtension} extension");
+                Console.WriteLine();
+
+        }
+
+        public void CountDirExtFiles(string fileExtension)
+        {
+
+                var fileCount = (from file in Directory.EnumerateFiles(path, fileExtension, SearchOption.TopDirectoryOnly)
+                                 select file).Count();
+                Console.WriteLine($"there is {fileCount} files in {path} with {fileExtension} extension");
+                Console.WriteLine();
+
+        }
+
+        public string ReturnPath()
+        {
+            return path;
+        }
+
+        public void SetPath(string path)
+        {
+            this.path = path;
+        }
+
+
+    }
+}
---
+++ Homework 1.16/Program.cs
@@ -0,0 +1,17 @@
+﻿using Homework_1._16;
+
+
+
+DirectoryWorker dw = new DirectoryWorker();
+
+dw.SetPath("F:\\TestDirectory\\");
+
+dw.FilesInDirectory();
+
+dw.DirectoryFileInfo();
+
+dw.CountCatExtFiles("*.txt");
+
+dw.CountDirExtFiles("*.txt");
+
+dw.FilesInCat();
