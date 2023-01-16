9d949380e5ad2c689954aeb167342cc33579e1cd
Ivan K <ki93@yandex.ru>
2023-01-16 22:50:10 +0300

    1.17

---
+++ Homework 1.17/Program.cs
@@ -0,0 +1,36 @@
+﻿using Homework_1._17;
+using System.IO;
+
+string dirPath = "F:\\TestDirectory2\\";
+
+var file1 = new FileInfo("Text1.txt");
+var file2 = new FileInfo("Text2.txt");
+var file3 = new FileInfo("Empty.txt");
+
+var writeStream1 = new FileStream($"{dirPath}{file1}", FileMode.Create, FileAccess.Write);
+var writeStream2 = new FileStream($"{dirPath}{file2}", FileMode.Create, FileAccess.Write);
+var writeStream3 = new FileStream($"{dirPath}{file3}", FileMode.Create, FileAccess.Write);
+
+
+string first = "some kind of text1";
+string second = "other kind of text2";
+
+StreamWriter sw1 = new StreamWriter(writeStream1);
+StreamWriter sw2 = new StreamWriter(writeStream2);
+
+sw1.WriteLine(first);
+sw2.WriteLine(second);
+
+sw1.Close();
+sw2.Close();
+writeStream1.Close();
+writeStream2.Close();
+writeStream3.Close();
+
+
+
+Thread t1 = new Thread(TextWorker.ActionOne);
+t1.Start();
+t1.Join();
+Thread t2= new Thread(TextWorker.ActionTwo);
+t2.Start();
---
+++ Homework 1.17/TextWorker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_1._17
+{
+    internal class TextWorker
+    {
+
+        public static void ActionOne()
+        {
+            Console.WriteLine("поток 1 запущен");
+            string dirPath = "F:\\TestDirectory2\\";
+            var file1 = new FileInfo("Text1.txt");
+            var file3 = new FileInfo("Empty.txt");
+
+            FileStream readStream = new FileStream($"{dirPath}{file1}", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(readStream);
+            string res = sr.ReadToEnd();
+
+            FileStream writeStream = new FileStream($"{dirPath}{file3}", FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(writeStream);
+            sw.WriteLine(res);
+            sw.Close();
+            writeStream.Close();
+        }
+
+        public static void ActionTwo()
+        {
+            Console.WriteLine("поток 2 запущен");
+            string dirPath = "F:\\TestDirectory2\\";
+            var file1 = new FileInfo("Text2.txt");
+            var file3 = new FileInfo("Empty.txt");
+
+            FileStream readStream1 = new FileStream($"{dirPath}{file1}", FileMode.Open, FileAccess.Read);
+            FileStream readStream2 = new FileStream($"{dirPath}{file3}", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(readStream1);
+            StreamReader sr2 = new StreamReader(readStream2);
+            string res = sr2.ReadToEnd();
+            res += sr.ReadToEnd();
+            sr.Close();
+            sr2.Close();
+
+            FileStream writeStream = new FileStream($"{dirPath}{file3}", FileMode.Open, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(writeStream);
+            sw.WriteLine(res);
+            sw.Close();
+            writeStream.Close();
+        }
+
+
+    }
+}
